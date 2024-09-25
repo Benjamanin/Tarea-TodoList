f6e3cf3eaf17f911740bad7c569e12426ae7df30
Benjamanin <112143465+Benjamanin@users.noreply.github.com>
2024-09-25 15:53:43 -0300

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+import '@mantine/core/styles.css';
+import { MantineProvider } from '@mantine/core';
+
+function App() {
+  return (
+    <MantineProvider>
+      {/* Despues de haber clonado el proyecto, el siguiente paso es hacer el "cd nuevo-proyecto" , luego el "npm install"
+      y para asi podras ejecutar el "npm run dev" sin ningun problema*/}
+    </MantineProvider>
+  );
+}
+
+export default App;
